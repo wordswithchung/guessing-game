08af616635c8d80381c97b19c9ca528461c6d799
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-27 12:53:30 -0700

    Rest of the game

--- game.py
+++ game.py
@@ -4,3 +4,18 @@ import random # import the random module
 player_name = raw_input("Hi! What's your name?\n")
 print "{}, I'm thinking of a number between 1 and 100.".format(player_name)
 print "Try to guess my number!"
+
+random_number_generated = randon.randint(1, 100)
+def guessing_game():
+	guess = int(raw_input("Your guess? "))
+	count = 0
+	if guess != random_number_generated:
+		count += 1
+		if guess > random_number_generated:
+			print "Your guess is too high! Try again!"
+		else:
+			print "Your guess is too low! Try again!"
+	else:
+		print "Congratulations, {}! You found the number in {} tries!".format(player_name, count)
+
+guessing_game()
