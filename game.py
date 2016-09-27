984a0f842989982d0fb4e64d4ff33539a8a06dae
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-27 12:45:22 -0700

    Greeting the player

---
+++ game.py
@@ -0,0 +1,6 @@
+# Put your code here
+import random # import the random module
+
+player_name = raw_input("Hi! What's your name?\n")
+print "{}, I'm thinking of a number between 1 and 100.".format(player_name)
+print "Try to guess my number!"
